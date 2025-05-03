38f59c0e2ca5acde7ad1bc2a4bd5a4c96a07a3ba
Anjali Singh <102979716+anjali-482@users.noreply.github.com>
2025-05-03 18:32:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    chunkSizeWarningLimit: 1500 // (optional: increase chunk warning limit)
+  },
+  preview: {
+    port: process.env.PORT || 4173, // 👈 important for Render
+    host: true                      // 👈 important for Render (listen on 0.0.0.0)
+  },
+  server: {
+    port: 3000,  // when you run locally using `npm run dev`
+    host: true
+  }
 })
